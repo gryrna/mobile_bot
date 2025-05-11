2748ee27c8530718bc0c96dfb427ad722b586b2f
gryrna <gumeshrana1@gmail.com>
2025-05-12 03:05:51 +0530

    saved empty world

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
 
     # Get the path to the world file
     world_file_path = os.path.join(
-        get_package_share_directory(package_name), "worlds", "training.world"
+        get_package_share_directory(package_name), "worlds", "empty.world"
     )
 
     # Launch Gazebo directly using gazebo_ros
