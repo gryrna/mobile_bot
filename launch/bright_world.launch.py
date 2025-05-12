a4db590960dbd5c8ecca7c51e200bc2569fba02b
gryrna <gumeshrana1@gmail.com>
2025-05-13 04:27:43 +0530

    modification for publishing to /model_states topic

--- launch/bright_world.launch.py
+++ launch/bright_world.launch.py
@@ -1,16 +1,13 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Including robot_state_publisher for our package.
-
     package_name = "mobile_bot"
 
     # Get the path to the world file
@@ -18,53 +15,48 @@ def generate_launch_description():
         get_package_share_directory(package_name), "worlds", "bright.world"
     )
 
-    # Launch Gazebo directly using gazebo_ros
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory("gazebo_ros"),
-                    "launch",
-                    "gazebo.launch.py",
-                )
-            ]
-        ),
-        launch_arguments={
-            "world": world_file_path,
-            "verbose": "true",
-        }.items(),
+    # Start gzserver with SystemPlugins
+    gzserver = ExecuteProcess(
+        cmd=[
+            "gzserver",
+            world_file_path,
+            "-s", "libgazebo_ros_init.so",
+            "-s", "libgazebo_ros_factory.so",
+            "--verbose"
+        ],
+        output="screen"
     )
 
-    # RSP launch
+    # Optional: Start gzclient (UI)
+    gzclient = ExecuteProcess(
+        cmd=["gzclient"],
+        output="screen"
+    )
+
+    # Robot State Publisher launch
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory(package_name), "launch", "rsp.launch.py"
-                )
-            ]
+            os.path.join(
+                get_package_share_directory(package_name), "launch", "rsp.launch.py"
+            )
         ),
         launch_arguments={"use_sim_time": "true"}.items(),
     )
 
-    # Spawn entity
+    # Spawn the robot in Gazebo
     spawn_entity = Node(
         package="gazebo_ros",
         executable="spawn_entity.py",
         arguments=[
-            "-topic",
-            "robot_description",
-            "-entity",
-            "mobile_bot"
+            "-topic", "robot_description",
+            "-entity", "mobile_bot"
         ],
         output="screen",
     )
 
-    # Launch them all
-    return LaunchDescription(
-        [
-            gazebo,
-            rsp,
-            spawn_entity,
-        ]
-    )
+    return LaunchDescription([
+        gzserver,
+        gzclient,
+        rsp,
+        spawn_entity
+    ])
